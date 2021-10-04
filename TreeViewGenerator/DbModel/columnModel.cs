6a98ba8792125aaa39858ac4324822b4220dcc46
gtk <work.eggpop@gmail.com>
2021-10-04 20:26:04 +0900

    update

---
+++ TreeViewGenerator/DbModel/columnModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class columnModel
+{
+    public string title { get; set; }
+    public bool effective { get; set; }
+    public string type { get; set; }
+    public string typeFix { get; set; }
+}
---
+++ TreeViewGenerator/DbModel/dbModel.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class dbModel
+{
+public string title { get; set; }
+public string dbPath { get; set; }
+}
