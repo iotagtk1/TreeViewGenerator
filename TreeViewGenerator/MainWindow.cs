7bdf60f3627cc29638d1f9304976520720a99e7d
gtk <work.eggpop@gmail.com>
2021-11-20 18:43:22 +0900

    update

--- TreeViewGenerator/MainWindow.cs
+++ TreeViewGenerator/MainWindow.cs
@@ -54,11 +54,15 @@ namespace TreeViewGenerator
             }
 
         }
-        
         private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
         {
             builder.Autoconnect(this);
+            DeleteEvent += Window_DeleteEvent;
+        }
 
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
         }
 
     }
